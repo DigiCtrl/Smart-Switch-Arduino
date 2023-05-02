693ae953960774ccd310c2a8ec2c586a823177cc
Taz03 <tanishazad03@gmail.com>
2023-05-02 18:47:42 +0530

    used Ethernet.maintain

--- SmartSwitch.ino
+++ SmartSwitch.ino
@@ -4,35 +4,45 @@
 #include <Ethernet.h>
 #include <EEPROM.h>
 
-byte inputs[] = { A0, A1, A2, A3, A4, A5, 2 };
-byte outputs[] = { 9, 8, 7, 6, 5, 4, 3 };
+byte inputs[] = { A0, A1, A2, A3, A4, A5 };
+byte outputs[] = { 9, 8, 7, 6, 5, 4 };
 byte ioLength = sizeof(inputs) / sizeof(inputs[0]);
 
-bool linked = false;
 EthernetServer server(23);
 EthernetClient client;
 
 void setup() {
     Serial.begin(9600);
 
+    pinMode(2, OUTPUT);
+
     for (byte i = 0; i < ioLength; i++) {
         pinMode(inputs[i], INPUT);
         pinMode(outputs[i], OUTPUT);
         digitalWrite(outputs[i], EEPROM.read(outputs[i]));
     }
+    
+    byte mac[6];
+    srand(time(NULL));
+    for (byte i = 0; i < 6; i++) {
+        if (EEPROM.read(1000 + i) == 0) {
+            EEPROM.update(1000 + i, rand() % 256);
+        }
 
-    if (Ethernet.linkStatus() == LinkON) {
-        connect();
+        mac[i] = EEPROM.read(1000 + i);
     }
+    
+    Ethernet.begin(mac);
+    Serial.println(Ethernet.localIP());
+    
+    server.begin();
+
+    digitalWrite(2, HIGH);
 }
 
 void loop() {
-    if (Ethernet.linkStatus() == LinkOFF) {
-        linked = false;
-    }
-
-    if (!linked && Ethernet.linkStatus() == LinkON) {
-        connect();
+    if (!(server)) {
+        server.begin();
     }
 
     EthernetClient newClient = server.accept();
@@ -71,23 +81,8 @@ void loop() {
             while (inRead(inputs[i]) > LOW) {}
         }
     }
-}
 
-void connect() {
-    byte mac[6];
-    srand(time(NULL));
-    for (byte i = 0; i < 6; i++) {
-        if (EEPROM.read(1000 + i) == 0) {
-            EEPROM.update(1000 + i, rand() % 256);
-        }
-
-        mac[i] = EEPROM.read(1000 + i);
-    }
-
-    linked = true;
-    Ethernet.begin(mac);
-    server.begin();
-    Serial.println(Ethernet.localIP());
+    Ethernet.maintain();
 }
 
 void sendIOLength() {
